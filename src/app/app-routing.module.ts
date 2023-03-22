cb67db6a79f9834e9877527148466a99ad069e41
phuql <quangphu112@gmail.com>
2023-03-22 22:04:33 +0700

    clear code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './modules/shared/components/page-not-found/page-not-found.component';
-import { AuthGuard } from './modules/shared/guards/auth.guard';
-import { PermissionGuard } from './modules/shared/guards/permission.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./modules/dashboard/pages/dashboard.module').then(m => m.DashboardModule),
-    canActivate: [AuthGuard]
   },
   {
     path: 'company',
     loadChildren: () => import('./modules/company/pages/company.module').then(m => m.CompanyModule),
-    canActivate: [PermissionGuard],
     
   },
   {
     path: 'division',
     loadChildren: () => import('./modules/division/pages/division.module').then(m => m.DivisionModule),
-    canActivate: [PermissionGuard]
   },
   {
     path: 'user',
